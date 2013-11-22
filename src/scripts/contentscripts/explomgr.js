8994860f8acf231b00ec7c6590b73ab956f17498
Sven Uhlig <hyperiums@resident-uhlig.de>
2013-11-22 01:09:41 +0100

    closes #2 add global input on Global exploitations manager

---
+++ src/scripts/contentscripts/explomgr.js
@@ -0,0 +1,12 @@
+$('#stdArray td:nth-child(4)').append(
+	$('<input maxlength="4" size="4">').
+		keydown(function (event) {
+			if (event.which == 13) {
+				event.preventDefault();
+			}
+		}).
+		on('input', function () {
+			$(this).closest('table').find(':not(#stdArray) input[type="text"]').val($(this).val());
+		})
+);
+
