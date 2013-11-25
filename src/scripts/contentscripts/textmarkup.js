d86bcba156a2a946a18e5f99a0d31801f6a39b50
Sven Uhlig <hyperiums@resident-uhlig.de>
2013-11-25 01:10:49 +0100

    added link to player search for fleet messages
    also put some markup code into separate functions

--- src/scripts/contentscripts/textmarkup.js
+++ src/scripts/contentscripts/textmarkup.js
@@ -5,13 +5,7 @@ var walker = document.createTreeWalker(
 	false
 );
 
-var MODE_TEXT = 0;
-var MODE_TABLE = 1;
-
-var node,
-	replacements = [], removals = [],
-	mode = MODE_TEXT,
-	table, numRows, hasHeader, totals, maxNumColumns = 0;
+var replacements = [], removals = [];
 
 function addReplacement(original, substitute) {
 	replacements.push({
@@ -28,18 +22,23 @@ function addRemoval(node) {
 }
 
 var reAbsNumber = /^\d+([\.,]\d+)? *([tbmk])?$/i,
-	reDate = /^\d\d\d\d\-\d\d\-\d\d \d\d:\d\d(:\d\d)?$/,
+	reDate = /^\d\d\d\d\-\d\d\-\d\d( \d\d:\d\d)?(:\d\d)?$/,
 	reOtherNumber = /\d+.*$/,
-	reUrl = /\b(https?:\/\/\S+[a-z0-9_\?\-#&=]|[a-z0-9\._%+\-]+@[a-z0-9\.\-]+\.[a-z]{2,6})/ig,
-	reHttp = /^https?:\/\//i,
+	reUrl = /\bhttps?:\/\/\S+[a-z0-9_\?\-#&=%]/ig
+	reEmail = /[a-z0-9\._%+\-]+@[a-z0-9\.\-]+\.[a-z]{2,6}/ig,
 	reColumns = /^([^\|]+ ?\| ?)+([^\|]+)$/i,
 	reHr = /^(\-\-+)|(==+)$/;
+	rePlayer = / player /g,
+	invalidParents = ['TEXTAREA', 'SCRIPT'];
 
 function textToValue(text) {
 	text = $.trim(text);
 	if (reAbsNumber.test(text)) {
 		return numeral().unformat(text.replace(',', '.').toLocaleLowerCase());
 	} else if (reDate.test(text)) {
+		if (text.length < 11) {
+			text += ' 00:00:00';
+		}
 		return new Date(text + ' +00:00');
 	} else if (reOtherNumber.test(text)) {
 		return 0;
@@ -47,114 +46,188 @@ function textToValue(text) {
 	return text;
 }
 
+function addNodes(nodes, re, callback) {
+	var createdNodes = [];
+	$.each(nodes, function (i, node) {
+		var matches, index = 0, string = node.nodeValue;
+		createdNodes[i] = [];
+		while (matches = re.exec(string)) {
+			index = callback(index, matches, createdNodes[i], string, node);
+		}
+		if (index) {
+			createdNodes[i].push(string.substring(index));
+		} else {
+			createdNodes[i] = node;
+		}
+	});
+	return createdNodes;
+}
+
+function addUrlLinks(nodes) {
+	return addNodes(nodes, reUrl, function (index, matches, createdNodes, string, node) {
+		var url = matches[0];
+
+		var a = document.createElement('a');
+		a.setAttribute('href', url);
+		a.setAttribute('target', 'blank');
+		a.appendChild(document.createTextNode(url));
+
+		createdNodes.push(string.substring(index, matches.index));
+		createdNodes.push(a);
+
+		return matches.index + url.length;
+	});
+}
+
+function addEMailLinks(nodes) {
+	return addNodes(nodes, reEmail, function (index, matches, createdNodes, string, node) {
+		var email = matches[0];
+
+		var a = document.createElement('a');
+		a.setAttribute('href', 'mailto:' + email);
+		a.appendChild(document.createTextNode(email));
+
+		createdNodes.push(string.substring(index, matches.index));
+		createdNodes.push(a);
+
+		return matches.index + email.length;
+	});
+}
+
+function addPlayerLinks(nodes) {
+	return addNodes(nodes, rePlayer, function (index, matches, createdNodes, string, node) {
+		var nextIndex = matches.index + matches[0].length, player = {};
+		if (nextIndex == string.length) {
+			player.name = $(node.nextSibling).text();
+			addRemoval(node.nextSibling);
+		} else {
+			player.name = string.substring(nextIndex, string.indexOf(' ', nextIndex));
+		}
+
+		var a = document.createElement('a');
+		a.setAttribute('href', Hyperiums7.getServletUrl('Player?page=Contacts&searchplayer=&playername=' + player.name));
+		a.appendChild(document.createTextNode(player.name));
+
+		createdNodes.push(string.substring(index, nextIndex));
+		createdNodes.push(a);
+
+		return nextIndex + player.name.length;
+	});
+}
+
+function addTextNodes(nodes) {
+	var span;
+	nodes = addUrlLinks(nodes);
+	nodes = addEMailLinks(nodes);
+	nodes = addPlayerLinks(nodes);
+	if (nodes[0] != node) {
+		span = document.createElement('span');
+		$.each(nodes, function (_, node) {
+			if (typeof node == 'string') {
+				span.appendChild(document.createTextNode(node));
+			} else if (node instanceof Array) {
+				$.each(node, arguments.callee);
+			} else {
+				span.appendChild(node);
+			}
+		});
+		addReplacement(node, span);
+	}
+}
+
+function addTableRow(table, node, totals) {
+	var text = node.nodeValue,
+		tr = $('<tr>').
+			addClass('line' + (table.find('tr').length % 2)).
+			mouseover(function () { $(this).addClass('lineCenteredOn'); }).
+			mouseout(function () { $(this).removeClass('lineCenteredOn'); });
+
+	$.each($.trim(text).split(/ ?\| ?/), function (i, text) {
+		var value = textToValue(text),
+			td = $('<td>');
+
+		if (typeof value == 'number') {
+			if (!totals[i]) {
+				totals[i] = 0;
+			}
+			totals[i] += value;
+			td.addClass('hr');
+		} else if (value instanceof Date) {
+			td.addClass('hc');
+			text = moment(value).utc().format('YYYY-MM-DD HH:mm');
+		}
+
+		tr.append(td.text(text));
+	});
+
+	table.append(tr);
+}
+
+function addTableTotals(table, totals) {
+	var tr = $('<tr class="stdArray">');
+
+	$.each(totals, function (i, value) {
+		var td = $('<td>');
+		if (!value && !i) {
+			td.text('Total');
+		} else if (value) {
+			td.addClass('hr').text(numeral(value).format('0[.]0a'));
+		}
+		tr.append(td);
+	});
+
+	var numColumns = table.find('tr:first-child').children().length;
+	while (tr.children().length < numColumns) {
+		tr.append('<td></td>');
+	}
+
+	table.append(tr);
+}
+
+var MODE_TEXT = 0,
+	MODE_TABLE = 1,
+	mode = MODE_TEXT,
+	node, table, totals;
 while (node = walker.nextNode()) {
-	if (node.parentNode.nodeName == 'TEXTAREA' ||
-		node.parentNode.nodeName == 'SCRIPT' ||
-		$(node).closest('table').parent().closest('table').prev('form').length
+	if (
+		-1 < $.inArray(node.parentNode.nodeName, invalidParents) ||
+		$(node).closest('table').parent().closest('table').prev('form').length // preview in forums
 	) {
 		continue;
 	}
-	var index = 0, match, span, a, url, tr,
-		text = node.nodeValue,
-		lineIsTable = reColumns.test(text);
 
-	if (node.parentNode.nodeName != 'PRE' &&
-		node.parentNode.nodeName != 'B' && (
-		lineIsTable || mode == MODE_TABLE
-	)) {
-		if (lineIsTable) {
+	(function (node) {
+		var text = node.nodeValue;
+		if (reColumns.test(text)) {
 			if (mode == MODE_TABLE) {
 				addRemoval(node);
 			} else {
 				mode = MODE_TABLE;
 				table = $('<table class="stdArray" width="100%">');
-				numRows = 0;
-				hasHeader = false;
 				totals = [];
 				addReplacement(node, table[0]);
 			}
-
-			tr = $('<tr>').
-				addClass('line' + (++numRows % 2)).
-				mouseover(function () { $(this).addClass('lineCenteredOn'); }).
-				mouseout(function () { $(this).removeClass('lineCenteredOn'); });
-
-			$.each($.trim(text).split(/ ?\| ?/), function (i, text) {
-				var value = textToValue(text), td;
-				td = $('<td>')
-				if (typeof value == 'number') {
-					if (!totals[i]) {
-						totals[i] = 0;
-					}
-					totals[i] += value;
-					td.addClass('hr');
-				} else if (value instanceof Date) {
-					td.addClass('hc');
-					text = moment(value).utc().format('YYYY-MM-DD HH:mm');
-				}
-				tr.append(td.text(text));
-			});
-
-			maxNumColumns = Math.max(maxNumColumns, tr.children().length);
-			table.append(tr);
-		} else if (reHr.test(text)) {
-			addRemoval(node);
-			if (!hasHeader) {
-				hasHeader = true;
-				table.find('tr').addClass('stdArray');
+		} else if (mode == MODE_TABLE) {
+			if (reHr.test(text)) {
+				addRemoval(node);
+				table.find('tr').eq(0).addClass('stdArray');
+				return;
 			}
-		} else {
 			if (totals.length) {
-				tr = $('<tr class="stdArray">');
-				$.each(totals, function (i, value) {
-					var td = $('<td>');
-					if (!value && !i) {
-						td.text('Total');
-					} if (value) {
-						td.addClass('hr').text(numeral(value).format('0[.]0a'));
-					}
-					tr.append(td);
-				});
-				table.append(tr);
-			}
-			while (tr.children().length < maxNumColumns) {
-				tr.append($('<td>'));
+				addTableTotals(table, totals);
 			}
 			mode = MODE_TEXT;
 		}
-	}
 
-	if (mode == MODE_TEXT) {
-		while (match = reUrl.exec(text)) {
-			if (!span) {
-				span = document.createElement('span');
-			}
-			span.appendChild(document.createTextNode(
-				text.substring(index, match.index)
-			));
-
-			url = match[0];
-			if (url.indexOf('@') > -1 && !reHttp.test(url)) {
-				url = 'mailto:' + url;
-			}
-
-			a = document.createElement('a');
-			a.setAttribute('href', url);
-			a.setAttribute('target', '_blank');
-			a.appendChild(document.createTextNode(match[0]));
-			span.appendChild(a);
-
-			index = match.index + match[0].length;
+		switch (mode) {
+		case MODE_TABLE:
+			addTableRow(table, node, totals);
+			break;
+		case MODE_TEXT:
+			addTextNodes([node]);
+			break;
 		}
-
-		if (index) {
-			span.appendChild(document.createTextNode(
-				text.substring(index)
-			));
-			addReplacement(node, span);
-			span = undefined;
-		}
-	}
+	})(node);
 }
 
 replacements.map(function (replacement) {
