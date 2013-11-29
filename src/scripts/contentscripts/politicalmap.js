8fa761201f2f2a8eac9e8cb4e48b72a4a1310e19
Sven Uhlig <hyperiums@resident-uhlig.de>
2013-11-29 14:50:02 +0100

    changed to use ajax cache for political map

--- src/scripts/contentscripts/politicalmap.js
+++ src/scripts/contentscripts/politicalmap.js
@@ -1,41 +1,45 @@
 $.getScript('/js/overlibmws.js').done(function () {
-	var cache = {}, jqXHR;
+	var isAborted = {};
 	$('.polMapZone, .polMapCenter').
 		mouseover(function() {
-			var url = $(this).find('a').attr('href');
-			if (cache[url]) {
-				overlib(cache[url]);
-			} else {
-				overlib('Loading...');
-				jqXHR = $.ajax(url).done(function (data) {
-					var planets = Hyperiums7.getPlanetsFromTradingMap(data),
-						rows = Hyperiums7.getStatisticsRowsFromPlanets(planets);
+			var url = Hyperiums7.getServletUrl($(this).find('a').attr('href'));
+			isAborted[url] = false;
+			overlib('Loading...');
+			jqXHR = chrome.runtime.sendMessage({
+				request: 'getAjaxCache',
+				url: url,
+				settings: {}
+			}, function (data) {
+				if (isAborted[url]) {
+					return;
+				}
+				var planets = Hyperiums7.getPlanetsFromTradingMap(data),
+					rows = Hyperiums7.getStatisticsRowsFromPlanets(planets);
 
-					rows.unshift($('<tr class="stdArray">').append([
-						'<td>Tag</td>',
-						'<td># Planets</td>',
-						'<td></td>', // coords
-						'<td>Civ.</td>',
-						'<td>Gov.</td>',
-						'<td>Race</td>',
-						'<td></td>', // distance
-						'<td>Product</td>',
-						'<td>Activity</td>',
-						'<td>Avail.<br>capacity</td>'
-					]));
+				rows.unshift($('<tr class="stdArray">').append([
+					'<td>Tag</td>',
+					'<td># Planets</td>',
+					'<td></td>', // coords
+					'<td>Civ.</td>',
+					'<td>Gov.</td>',
+					'<td>Race</td>',
+					'<td></td>', // distance
+					'<td>Product</td>',
+					'<td>Activity</td>',
+					'<td>Avail.<br>capacity</td>'
+				]));
 
-					var table = $('<table class="stdArray">').
-						css('white-space', 'nowrap').
-						append($('<tbody>').append(rows));
+				var table = $('<table class="stdArray">').
+					css('white-space', 'nowrap').
+					append($('<tbody>').append(rows));
 
-					table.find('td').css('font-size', 'smaller');
-					cache[url] = table[0].outerHTML;
-					overlib(cache[url]);
-				});
-			}
+				table.find('td').css('font-size', 'smaller');
+				overlib(table[0].outerHTML);
+			});
 		}).
 		mouseout(function () {
-			jqXHR.abort();
+			var url = Hyperiums7.getServletUrl($(this).find('a').attr('href'));
+			isAborted[url] = true;
 			nd();
 		});
 });
