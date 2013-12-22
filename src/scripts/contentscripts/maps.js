972e29a788ecad375e2f9fcab33db500c886f347
Sven Uhlig <hyperiums@resident-uhlig.de>
2013-12-22 13:20:03 +0100

    changed from text check to href check

--- src/scripts/contentscripts/maps.js
+++ src/scripts/contentscripts/maps.js
@@ -1,18 +1,17 @@
 (function () {
-	if ($('.megaCurrentItem').text() != 'Trading') {
-		return;
+	if ($('.megaCurrentItem').attr('href') == '/servlet/Maps?maptype=planets_trade') {
+		$('[name="searchplanets"]').keydown(function (event) {
+			if (event.which == 13) {
+				event.preventDefault();
+				$('#searchButton').click();
+			}
+		});
+
+		var planets = Hyperiums7.getPlanetsFromTradingMap(document);
+
+		$('table.stdArray').append(
+			$('<tfoot>').append(Hyperiums7.getStatisticsRowsFromPlanets(planets))
+		);
 	}
-	$('[name="searchplanets"]').keydown(function (event) {
-		if (event.which == 13) {
-			event.preventDefault();
-			$('#searchButton').click();
-		}
-	});
-
-	var planets = Hyperiums7.getPlanetsFromTradingMap(document);
-
-	$('table.stdArray').append(
-		$('<tfoot>').append(Hyperiums7.getStatisticsRowsFromPlanets(planets))
-	);
 })();
 
