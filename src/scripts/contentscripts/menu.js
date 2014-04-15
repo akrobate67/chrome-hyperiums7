00969e7f99c93444ca69b8c0c103b6f43307c5ba
Sven Uhlig <hyperiums@resident-uhlig.de>
2014-04-15 22:22:53 +0200

    added fleet spread sheet

--- src/scripts/contentscripts/menu.js
+++ src/scripts/contentscripts/menu.js
@@ -1,6 +1,9 @@
-$('#militarySubmenu ul').append([
-	$('<li>').append($('<a target="_blank">Fleet calculator</a>').attr({
-		href: chrome.runtime.getURL('pages/fleetcalc.html')
-	}))
-]);
+$(document).ready(function () {
+	$('#militarySubmenu ul').append([
+		$('<li>').append($('<a target="_blank">Fleet calculator</a>').attr({
+			href: chrome.runtime.getURL('pages/fleets/calc.html') })),
+		$('<li>').append($('<a target="_blank">Fleet spread sheet</a>').attr({
+				href: chrome.runtime.getURL('pages/fleets/spreadsheet.html') }))
+	]).closest('.megawrapper').css('height', '+=30px');
+});
 
