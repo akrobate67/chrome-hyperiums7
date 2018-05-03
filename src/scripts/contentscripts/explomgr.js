8f039f6bd4c36d78032c681e8cd2a9c637a7c7ee
Rémy Laligand <19413392+Laligand@users.noreply.github.com>
2018-05-03 18:15:04 +0200

    Update explomgr.js
    Input placeholder : exp to buy for pop/10 ratio

--- src/scripts/contentscripts/explomgr.js
+++ src/scripts/contentscripts/explomgr.js
@@ -14,9 +14,13 @@ $('form #stdArray td:nth-child(4)').append(
 
 Hyperiums7.getPlanetInfo().done(function (planets) {
 	$.each(planets, function (_, planet) {
-		$('[href="Planetprod?planetid=' + planet.id + '"]').closest('tr').
-			children('td').eq(2).append('<br>Population size: ',
-				numeral(planet.pop).format('0,0'), '&nbsp;M')
+		var plDetails = $('[href="Planetprod?planetid=' + planet.id + '"]').closest('tr').children('td');
+		plDetails.eq(2).append('<br>Population size: ',
+				numeral(planet.pop).format('0,0'), '&nbsp;M');
+		var exp = numeral(plDetails.eq(1).find('tr').children('td').eq(1).text());
+		var bought = numeral(plDetails.eq(2).find('tr').children('td').eq(1).text()) || 0;
+		var opti = Math.floor(numeral(planet.pop)/100)*10 - exp - bought;
+		plDetails.eq(3).find('input').attr('placeholder', opti);
 	});
 });
 
